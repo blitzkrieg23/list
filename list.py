2666b5e1da3cdc587636d591c92976338700a686
xzqt23 <angelo.agualada@gmail.com>
2023-02-16 17:35:38 +0800

    Create list.py

---
+++ list.py
@@ -0,0 +1,10 @@
+# List is a collection which is ordered and changeable. Allows duplicate members.
+
+# list = ["abc", 21, True, 40.5, "male"]  #--list with various data types
+
+
+fruits = ["🍎","🍍","🍋"]
+
+print(fruits[0]) # access individual list
+print(fruits)
+print(len(fruits))          
